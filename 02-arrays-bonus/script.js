038b64e9cc40305fe313b94a08afa032d619aee0
manuelpezzi <pezzimanuel@gmail.com>
2025-01-13 12:41:19 +0100

    bonus(3)

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -15,9 +15,16 @@ console.log(teachers)
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = teachers.filter(teacher => teacher.length >= 5);
-console.log(longNames)
+const longNames = /*teachers.filter(teacher => teacher.length >= 5);
+console.log(longNames)*/
 
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i].length >= 5) {
+    longNames.push(teachers[i]);
+  }
+
+}
+console.log(longNames)
 // 3. Rimuovi 'Ed' dall'array teachers
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
