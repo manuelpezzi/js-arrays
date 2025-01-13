1047ae8998d487231d5f7ec4f2a0aac874e11bd7
manuelpezzi <pezzimanuel@gmail.com>
2025-01-13 11:55:15 +0100

    fourth array solved

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -21,9 +21,11 @@ const lastTeacher = teachers.pop();
 console.log(teachers)
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
+console.log(teachers)
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
