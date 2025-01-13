1814cea2fe57de5f3c5edcd6c8b318578271ca25
manuelpezzi <pezzimanuel@gmail.com>
2025-01-13 12:58:27 +0100

    fine bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -25,10 +25,13 @@ for (let i = 0; i < teachers.length; i++) {
 }
 console.log(longNames)
 // 3. Rimuovi 'Ed' dall'array teachers
-
+teachers.splice(5, 1)
+console.log(teachers)
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.includes("Fabio");
+console.log(isFabioPresent)
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.join();
+console.log(teachersString)
